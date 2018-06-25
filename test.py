f2ce6a54cd0a0eb32eca6b75dedbdfc22050a5c1
Nitish <nitishojha33@gmail.com>
2018-06-25 17:25:49 +0530

    second change

---
+++ test.py
@@ -0,0 +1,24 @@
+#a = [10, 20, 'abc', 3.2, [1, 2, 3]]
+#print(a[4][2])
+#a[2]= a[2]+ "de"
+#print(a[2])
+#a[2]= a[2]- 'e'
+#print(a[2])
+#print('4'+ '2')
+mydict1= {'name' : 'modi',
+           'post' : 'pm',
+           'age': 60,
+           'party': 'bjp',
+         }
+mydict2= {'name' : 'obama',
+           'post' : 'ex president',
+           'age': 50,
+           'party': 'desuocanatic',
+         }
+mydict3= {'name' : 'tump',
+           'post' : 'president',
+           'age': 70,
+           'party': 'republican',
+         }
+mylist= [mydict1, mydict2, mydict3]
+print(mylist[1]['name'])                 
